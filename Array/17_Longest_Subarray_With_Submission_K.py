eab04d88f0b827e7e1fe83c281579a0fbff29db6
Piyush Thaware <Piyushbthaware@gmail.com>
2024-07-05 13:01:00 +0530

    Add : Array

--- Array/17_Longest_Subarray_With_Submission_K.py
+++ Array/17_Longest_Subarray_With_Submission_K.py
@@ -12,20 +12,26 @@ class Solution:
     def brute(self,arr,k):
         # step-1 --> generate all the sub-array
         # step-2 --> check the len of each sub-array if it is equal to k means we got the longest sub-array.
-        sub_arrays = []
-        for i in range(0,len(arr)):
-            for j in range(i,len(arr)):
-                # print(sub_arrays)
-                sub_arrays.append(arr[i:j+1])
-        for m in range(0,len(sub_arrays)):
-            if len(sub_arrays[m]) == k:
-                print(sub_arrays[m])
-                return
+        longest = 0
+        for i in range(0, len(arr)):
+            for j in range(i, len(arr)):
+                submission = 0
+                for m in range(i, j):
+                    submission = submission + arr[m]
+                    # print(arr[m], end=" ")
+                if submission == k:
+                    longest = max(longest, j - i + 1)
+        print(longest)
+                    
                 
 
 
         
-nums = [1,2,3,1,1,1,4,2,3]
-k = 3
+nums = [2,3,5,1,9]
+k = 10
 s = Solution()
 s.brute(nums,k)
+
+nums = [2,3,5,1,9]
+for j in range(len(nums)-1,-1,-1):
+    print(nums[j])
