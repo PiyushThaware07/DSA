9fd3409ad1bb10ded92bf8b8b7f2e31883cfe0a3
Piyush Thaware <Piyushbthaware@gmail.com>
2024-07-08 14:24:34 +0530

    Add : String

--- String/04_Longest_Common_Prefix.py
+++ String/04_Longest_Common_Prefix.py
@@ -15,10 +15,59 @@ Explanation: There is no common prefix among the input strings.
 '''
 
 class Solution:
-    def brute(self,arr):
-       
+    def brute(self,words):
+        # if words is empty
+        if not words:
+            print("Empty")
+
+        prefix = words[0]
+        for i in range(1,len(words)):
+            word = words[i] 
+            i = 0
+            while i<len(prefix) and i<len(word) and prefix[i] == word[i]:
+                i = i+1
+            prefix = prefix[0:i]
+
+        if prefix == "":
+            print("There is no common prefix")
+        else:
+            print(prefix)
+
+    def optimized(self,words):
+        '''
+        steps :
+        1. sort the words
+        2. take first and last word in variable
+        3. iterate first and last word and compare each char of first and last whenever the character matches add into the result if not matches the break
+        4. return the result.
+        '''
+        # print("Before Sorting --> ",words)
+        words.sort()
+        # print("After Sorting --> ",words)
+        firstElement = words[0]
+        lastElement = words[len(words)-1]
+        # print(f"FirstElement --> {firstElement}\nLastElement --> {lastElement}")
+
+        # If array of string is empty
+        if not words:
+            print("Empty")
+
+        prefix = ""
+        for i in range(0,len(firstElement)):
+            if firstElement[i] == lastElement[i]:
+                prefix = prefix + firstElement[i]
+            else:
+                break
+        # if there is no common prefix
+        if prefix == "":
+            print("There is no common prefix")
+        else:
+            print(prefix)
+
+                
 
 
 myarr = ["flower","flow","flight"]
 s = Solution()
 s.brute(myarr)
+s.optimized(myarr)
