8f88bae0b82e387256f877e6b12a82504450e178
Piyush Thaware <Piyushbthaware@gmail.com>
2024-08-07 12:33:13 +0530

    add : linked list

---
+++ Linked List/Singly Linked List/09_RemoveDublicates.py
@@ -0,0 +1,30 @@
+'''
+Remove Dublicates From Linked List
+'''
+
+from custom.linkedlist import CreateNode, LinkedList
+
+class Solution:
+    def optimize(self,myList):
+        head = myList.head
+        current = head
+        while current and current.next:
+            if current.data == current.next.data:
+                current.next = current.next.next 
+            else:
+                current = current.next 
+        myList.head = head 
+        myList.traversal()
+
+            
+
+
+
+ll1 = LinkedList()
+ll1.addEnd(1)
+ll1.addEnd(1)
+ll1.addEnd(2)
+ll1.traversal()
+
+s = Solution()
+s.optimize(ll1)
